e81f169e6485e9efd75b0233a31784d5189a1dbd
Ilma Rizwi <ilmarizwi@Ilmas-MacBook-Air.local>
2026-07-04 12:34:32 +0530

    added day_24_3.c

---
+++ day_24_2.c
@@ -0,0 +1,27 @@
+
+// question:write a program to compress a string.
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, j = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != str[i + 1])
+        {
+            str[j] = str[i];
+            j++;
+        }
+    }
+
+    str[j] = '\0';
+
+    printf("Compressed string: %s", str);
+
+    return 0;
+}
